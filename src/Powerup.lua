3aa335898aadf83c92adda3698d94bedaaa6d579
Hasham Asad <tipuasad2001@gmail.com>
2024-06-10 22:02:38 +0500

    powerUpClass

---
+++ src/Powerup.lua
@@ -0,0 +1,21 @@
+
+
+
+
+Powerup = Class{}
+
+function Powerup:init()
+    
+end
+
+function Powerup:catched()
+    
+end
+
+function Powerup:render()
+    
+end
+
+function Powerup:update()
+    
+end
